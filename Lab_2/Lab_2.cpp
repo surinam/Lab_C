3f8843be0614b91bdfaef49cf61802aca7f9e916
surinam <aruba223@mail.ru>
2015-09-17 23:01:07 +0300

    опис. демонстр. файла

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -13,6 +13,27 @@ using namespace  std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	setlocale (LC_CTYPE,"rus");
+	Tovar tov;
+	tov.setBrend("Apple");
+	tov.getBrend ();
+	tov.setSkidka(true);
+	tov.getSkidka();
+	tov.setStoimost(2698000);
+	tov.getStoimost();
+	tov.setTip_tovara("Бытовая техника");
+	tov.getTip_tovara ();
+
+	Skaner scan;
+	scan.setBrend ("Epson");
+	scan.getBrend ();
+	scan.setSkidka (false);
+	scan.getSkidka ();
+	scan.setStoimost (5520000);
+	scan.getStoimost ();
+	scan.setWeight (2.8);
+	scan.getWeight ();
+
 	return 0;
 }
 
